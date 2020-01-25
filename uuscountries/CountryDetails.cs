b58e3d4aef4aea5130a2edaddd3410c1042dbe6c
Aet Sorokolet <aetsorokolet@gmail.com>
2020-01-25 16:44:01 +0200

    .

---
+++ uuscountries/CountryDetails.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace uuscountries
+{
+    class CountryDetails
+    {
+        
+        public class Language
+        {
+            public string Name { get; set; }
+        }
+
+        public class Country
+        {
+            public string Name { get; set; }
+
+            public string Capital { get; set; }
+
+            public string Region { get; set; }
+
+            public string Cioc { get; set; }
+
+            public int Population { get; set; }
+
+            public List<string> TopLevelDomain { get; set; }
+
+            public List<Language> Languages { get; set; }
+        }
+    }   
+}
+
---
+++ uuscountries/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace uuscountries
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://restcountries.eu/rest/v2/name/eesti";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                //kontroliks:
+                //Console.WriteLine(response);
+                var countries = JsonConvert.DeserializeObject<List<CountryDetails.Country>>(response);
+                foreach (var maa in countries)
+                {
+                    Console.WriteLine($"nimi: {maa.Name}");
+                    Console.WriteLine($"pealinn: {maa.Capital}");
+                    Console.WriteLine($"cioc: {maa.Cioc}");
+                    Console.WriteLine($"regioon: {maa.Region}");                    
+                    Console.WriteLine($"elanikkond: {maa.Population}");
+                    
+                    foreach (var keel in maa.Languages)
+                    {
+                        Console.WriteLine($"keel:{keel.Name}");
+                    }
+                    foreach (var domeen in maa.TopLevelDomain)
+                    {
+                        Console.WriteLine($"domeen:{maa.TopLevelDomain[0]}");
+                    }
+                }            
+
+
+
+            }
+        }
+    }
+}
